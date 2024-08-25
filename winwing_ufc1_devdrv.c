596a5475236cc36728bc8f0812233376e6773499
Giorgos Makris <makris.giwrgos1234@gmail.com>
2024-08-25 10:49:48 +0300

    Convert to HID driver from USB

--- winwing_ufc1_devdrv.c
+++ winwing_ufc1_devdrv.c
@@ -1,6 +1,7 @@
 #include <linux/module.h>
 #include <linux/init.h>
-#include <linux/usb.h>
+#include <linux/hid.h>
+#include <linux/hidraw.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Giorgos Makris");
@@ -9,44 +10,32 @@ MODULE_DESCRIPTION("Driver for the WinWing UFC1");
 #define VENDOR_ID 0x4098
 #define PRODUCT_ID 0xbed0
 
-static struct usb_device_id usb_table[] = {
-  {USB_DEVICE(VENDOR_ID, PRODUCT_ID)},
+static struct hid_device_id device_table[] = {
+  {HID_USB_DEVICE(VENDOR_ID, PRODUCT_ID)},
   {},
 };
 
-MODULE_DEVICE_TABLE(usb, usb_table);
+MODULE_DEVICE_TABLE(hid, device_table);
 
-static int probe(struct usb_interface *intf, const struct usb_device_id *id) {
+static int probe(struct hid_device *hdev, const struct hid_device_id *id) {
   printk("winwing_ufc1_devdrv - Probed\n");
   return 0;
 }
 
-static void disconnect(struct usb_interface *intf){
-  printk("winwing_ufc1_devdrv - Disconnected\n");
-}
-
-static struct usb_driver driver = {
-  .name = "winwing_ufc1_devdrv",
-  .id_table = usb_table,
-  .probe = probe,
-  .disconnect = disconnect
-};
-
-static int __init ww_ufc_init(void) {
-  int registered = usb_register(&driver);
-
-  if (registered) {
-    printk("winwing_ufc1_devdrv - Error: could not register driver!\n");
-    return -registered;
-  }
-  printk("winwing_ufc1_devdrv - Initialized driver\n");
+static int input_configured(struct hid_device *hdev, struct hid_input *hidinput) {
   return 0;
 }
 
-static void __exit ww_ufc_exit(void) {
-  usb_deregister(&driver);
-  printk("winwing_ufc1_devdrv - Unloaded driver\n");
+static int raw_event(struct hid_device *hdev, struct hid_report *report, u8 *raw_data, int size) {
+  return 0;
 }
 
-module_init(ww_ufc_init);
-module_exit(ww_ufc_exit);
+static struct hid_driver ww_ufc1_driver = {
+  .name = "winwing_ufc1_devdrv",
+  .id_table = device_table,
+  .probe = probe,
+  .input_configured = input_configured,
+  .raw_event = raw_event
+};
+
+module_hid_driver(ww_ufc1_driver);
